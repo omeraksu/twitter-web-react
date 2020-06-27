bbc298ca600f4cdb879056149d9710f000aadbee
Ömer Aksu <omeeraksu@gmail.com>
2020-06-27 23:34:05 +0300

    postcss ekledik

--- components/button.js
+++ components/button.js
@@ -1,1 +1,1 @@
-import React from 'react'import styles from './button.module.css'function Button({ children }) {  return (    <button type="button" className={styles.button}>      {children}    </button>  )}export default Button
+import React from 'react'import styles from './button.module.css'function Button({ children, ...props }) {  return (    <button type="button" className={styles.button} {...props}>      {children}    </button>  )}export default Button
---
+++ components/nav-button.js
@@ -0,0 +1,1 @@
+import React from 'react'import Button from './button'import styles from './nav-button.module.css'function NavButton({ children }) {  return <Button className={styles.navButton}>{children}</Button>}export default NavButton
