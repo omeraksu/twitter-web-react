7e4280905d710b081d587116935ff423066cf094
Ömer Aksu <omeeraksu@gmail.com>
2020-06-27 22:15:46 +0300

    prettier, svg ayarlandı

---
+++ stories/0-SlmKnki.stories.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default {
+  title: 'Welcome'
+}
+
+export const SlmCanim = () => (
+  <div className="selam">
+    <h1>Selam kankiler</h1>
+    <p>
+      Twitter sitesinde biraz garipsediğim, "ya olsa nasıl olurdu" dediğim
+      özellikleri ekleme fırsatı bulduğum bu çalışmayı başlattığı için
+      @ademilter'e minnetim sonsuz.
+    </p>
+  </div>
+)
+
+SlmCanim.story = {
+  name: 'Mrb cnm.'
+}
